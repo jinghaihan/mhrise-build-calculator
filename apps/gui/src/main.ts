e3707115e9eee7da673092905dbce7fedf118f4a
jinghaihan <jhh19980114@gmail.com>
2026-09-07 22:52:20 +0800

    fix: install gui i18n provider

--- apps/gui/src/main.ts
+++ apps/gui/src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { i18n } from './i18n'
 import '@unocss/reset/tailwind.css'
 import '@antfu/design/styles/base.css'
 import '@antfu/design/styles/scrollbar.css'
 import 'uno.css'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
